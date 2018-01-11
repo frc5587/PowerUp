5dac0c8db5200a6ec538c32b5687f0f588304644
OwenTC <OwenTC@users.noreply.github.com>
2018-01-11 15:34:18 -0500

    Added subsystem and new robot map classes

---
+++ src/main/java/org/frc5587/robot2018/subsystems/Pneumatics.java
@@ -0,0 +1,16 @@
+package org.frc5587.robot2018.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Pneumatics extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+
+    public void initDefaultCommand() {
+        // TODO: Set the default command, if any, for a subsystem here. Example:
+        //    setDefaultCommand(new MySpecialCommand());
+    }
+}
+
