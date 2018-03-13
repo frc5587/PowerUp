a134d399ce472681f1431b3f8a5d7b91eb771b36
Nathan E. Egge <negge@dgql.org>
2018-03-13 18:14:57 -0400

    Add tool to visualize generated Trajectory's.

---
+++ tools/main/java/org/frc5587/lib/PathToSVG.java
@@ -0,0 +1,63 @@
+package org.frc5587.lib;
+
+import org.frc5587.lib.Pathgen;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Trajectory.Segment;
+import jaci.pathfinder.Waypoint;
+
+public class PathToSVG {
+
+  public static String HTML_PROLOG =
+    "<html>\n" +
+    "  <head>\n" +
+    "    <style type=\"text/css\">\n" +
+    "      svg {\n" +
+    "        width: 1200;\n" +
+    "        height: 1200;\n" +
+    "      }\n" +
+    "      polygon {\n" +
+    "        stroke: grey;\n" +
+    "        stroke-width: 1;\n" +
+    "      }\n" +
+    "      polyline {\n" +
+    "        stroke: grey;\n" +
+    "        stroke-width: 1;\n" +
+    "      }\n" +
+    "    </style>\n" +
+    "  </head>\n" +
+    "  <body>\n" +
+    "    <svg>\n" +
+    /* bottom left switch */
+    "      <polyline points=\"140.000,83.250 196.000,83.250\"/>\n" +
+    "      <polyline points=\"196.000,83.250 196.000,119.250\"/>\n" +
+    "      <polyline points=\"196.000,119.250 140.000,119.250\"/>\n" +
+    "      <polyline points=\"140.000,119.250 140.000,83.250\"/>\n" +
+    /* top left switch */
+    "      <polyline points=\"140.000,238.750 196.000,238.750\"/>\n" +
+    "      <polyline points=\"196.000,238.750 196.000,202.750\"/>\n" +
+    "      <polyline points=\"196.000,202.750 140.000,202.750\"/>\n" +
+    "      <polyline points=\"140.000,202.750 140.000,238.750\"/>";
+
+  public static String HTML_EPILOG =
+    "    </svg>\n" +
+    "  </body>\n" +
+    "</html>";
+
+  public static void main(String[] args) {
+    Pathgen fastPath = new Pathgen(30, 0.010, 84, 80, 160);
+    Trajectory t = fastPath.createTrajectory(new Waypoint[] {
+      new Waypoint(39, 268, Pathfinder.d2r(0)),
+      new Waypoint(144, 268, Pathfinder.d2r(0)),
+      new Waypoint(180, 230, Pathfinder.d2r(-89))
+    });
+    System.out.println(HTML_PROLOG);
+    for (int i = 0; i < t.segments.length - 1; i++) {
+      Segment s = t.segments[i];
+      Segment e = t.segments[i + 1];
+      System.out.println("      <polyline points=\"" + s.x + "," + s.y + " " + e.x + "," + e.y + "\"/>");
+    }
+    System.out.println(HTML_EPILOG);
+  }
+
+};
